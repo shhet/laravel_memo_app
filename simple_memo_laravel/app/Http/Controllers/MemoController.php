20322c10ee57c1944317324ca291b91a9bbea1e0
shhet <agwndm2401@gmail.com>
2023-04-26 23:59:59 +0900

    add memo adding

---
+++ simple_memo_laravel/app/Http/Controllers/MemoController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Memo;
+use Illuminate\Support\Facades\Auth;
+
+class MemoController extends Controller
+{
+    /**
+     * 初期表示
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index()
+    {
+        return view('memo');
+    }
+
+    /**
+     * メモの追加
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function add()
+    {
+        Memo::create([
+            'user_id' => Auth::id(),
+            'title' => '新規メモ',
+            'content' => '',
+        ]);
+
+        return redirect()->route('memo.index');
+    }
+
+}
